fde8098ab85c4dc16c98c8739c0b42f946fadded
Anubhav Kandiyal <anubhav.kandiyal@agrostar.in>
2024-05-18 22:58:50 +0530

    Launching one activity from another using intent

--- app/src/main/java/com/example/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -1,43 +1,19 @@
 package com.example.notekeeper
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.notekeeper.databinding.ActivityNoteListBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NoteListActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityNoteListBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
 
-        binding = ActivityNoteListBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_note_list)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
+        val fab: FloatingActionButton = findViewById(R.id.floatingActionButton3)
+        fab.setOnClickListener {
+            val activityIntent = Intent(this, SecondActivity::class.java)
+            startActivity(activityIntent)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_note_list)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
