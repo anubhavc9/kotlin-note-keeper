f96813de7e497371326a644d4d98ac04719a4b29
Anubhav Kandiyal <anubhav.kandiyal@agrostar.in>
2024-05-19 13:05:28 +0530

    retrieving the position of the clicked note from NoteListActivity in SecondActivity. Using the note position to display the selected note by the user.

--- app/src/main/java/com/example/notekeeper/SecondActivity.kt
+++ app/src/main/java/com/example/notekeeper/SecondActivity.kt
@@ -3,9 +3,13 @@ package com.example.notekeeper
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
+    private var notePosition = POSITION_NOT_SET
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -22,5 +26,30 @@ class SecondActivity : AppCompatActivity() {
         // associate the adapter with the Spinner
         val spinnerCourses: Spinner = findViewById(R.id.spinnerCourses)
         spinnerCourses.adapter = adapterCourses
+
+        // retrieving the note position from Intent Extra
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+        // if the intent that starts this activity has passed position, it means user want to edit an existing note
+        // if no position is passed in Intent Extra, user wants to add a new note
+        if (notePosition != POSITION_NOT_SET)
+            displayNote()
+    }
+
+    private fun displayNote() {
+        // retrieving the clicked note from DataManager
+        val note = DataManager.notes[notePosition]
+
+        // displaying values within the views on our activity
+        val textNoteTitle: TextView = findViewById(R.id.textNoteTitle)
+        textNoteTitle.setText(note.title)
+
+        val textNoteText: TextView = findViewById(R.id.textNoteText)
+        textNoteText.setText(note.text)
+
+        // select the matching course within the Spinner
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        val spinnerCourses: Spinner = findViewById(R.id.spinnerCourses)
+        spinnerCourses.setSelection(coursePosition)
+
     }
 }
